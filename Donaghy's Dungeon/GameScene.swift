77ab332d579df518854afd092a1ab8342f79ac2c
HSDeCecco <beeHSD2720@gmail.com>
2017-12-15 12:13:55 -0500

    jkl;jkl;jkl;

--- Donaghy's Dungeon/GameScene.swift
+++ Donaghy's Dungeon/GameScene.swift
@@ -15,14 +15,21 @@ class GameScene: SKScene {
     let smallFont = 20
     let font = "Luminari"
     
+    
     override func didMove(to view: SKView) {
+        wait(time: 1.0)
         makeLabel(text: "Donaghy's", fontSize: bigFont, font: font, lines: 1, postionX: Double(frame.width / 2), postionY: Double(frame.height * 0.75))
-     
+        wait(time: 2.0)
+        makeLabel(text: "Dungeon", fontSize: bigFont, font: font, lines: 1, postionX: Double(frame.width / 2), postionY: Double(frame.height * 0.5))
+        wait(time: 4.0)
+        makeLabel(text: "Click to Begin", fontSize: smallFont, font: font, lines: 1, postionX: Double(frame.width / 2), postionY: Double(frame.height * 0.4))
+        
     }
     
     
     
     func makeLabel(text: String, fontSize: Int, font: String, lines: Int, postionX: Double, postionY: Double){
+        
         let label = SKLabelNode(fontNamed: font)
         label.position = CGPoint(x: postionX, y: postionY)
         label.text = text
@@ -30,7 +37,12 @@ class GameScene: SKScene {
         label.fontSize = CGFloat(fontSize)
         label.fontColor = UIColor.yellow
         addChild(label)
+        
     }
     
+    func wait(time: Double){
+        let wait = SKAction.wait(forDuration: time)
+        run(wait)
+    }
     
 }
